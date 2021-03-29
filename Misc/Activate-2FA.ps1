e25d763abaec5da81d6cdbf6f427394b4d33ad97
Moritz Engel <moritz.engel@bdsu.de>
2021-03-29 19:25:22 +0200

    [Misc] Skript für Aktivieren der 2 FA hinzugefügt

---
+++ Misc/Activate-2FA.ps1
@@ -0,0 +1,45 @@
+﻿<#################################################################################################################
+
+Skript um im Tenant für Accounts die 2FA zu aktivieren
+Quelle/Referenz: https://docs.microsoft.com/de-de/azure/active-directory/authentication/howto-mfa-userstates
+
+Voraussetzung:
+- Installation des Exchange Online Moduls V2
+  (siehe https://www.powershellgallery.com/packages/ExchangeOnlineManagement/ )
+
+- Installation des MSOnline Modul
+  (Befehl: Install-Module MSOnline)
+  (siehe https://docs.microsoft.com/de-de/azure/active-directory/authentication/howto-mfa-userstates#change-state-using-powershell)
+
+
+
+################################################################################################################>
+
+# Mit MSOnline verbinden
+Connect-MsolService
+
+# Alle Accounts erfassen
+$alleAccounts = Get-AzureADUser -All $true
+
+# Mögliche Filter:
+# Gastaccounts rausfiltern
+$alleNutzer = $alleAccounts | Where-Object {$_.UserType -eq "Member"}
+
+# Accounts überprüfen
+#$alleNutzer | ft displayname, usertype
+
+# 2 FA für alle Accounts aktivieren 
+foreach ($user in $alleNutzer)
+{
+    
+    $st = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+    $st.RelyingParty = "*"
+    $st.State = "Enabled"
+    $sta = @($st)
+    Set-MsolUser -UserPrincipalName $user.UserPrincipalName -StrongAuthenticationRequirements $sta
+    Write-Host $user.displayname " wurde fuer 2 FA enabled"
+}
+
+
+
+    
--- Misc/README.md
+++ Misc/README.md
@@ -1,2 +1,5 @@
 ### [Delete-Mail-Globally.ps1](Delete-Mail-Globally.ps1)
 Skript, um eingenangene und nicht gefilterte Phishing-Emails aus allen vorhandenen Postfächern im MS365 Tenant zu entfernen.
+
+### [Activate-2FA.ps1](Activate-2FA.ps1)
+Skript, um die 2 FA für Accounts im Office 365 Tenant zu aktivieren.
